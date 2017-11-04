b325ea6c2b57d43c52286f851747118252093dbb
samvanlier <sam.vanlier@student.kdg.be>
2017-11-04 19:33:14 +0100

    aanpassingen blog_v6

--- Blog_v6/src/main/java/be/kdg/blog/config/ApplicationSecurityConfig.java
+++ Blog_v6/src/main/java/be/kdg/blog/config/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package be.kdg.blog.config;
 
+import be.kdg.blog.security.LoginSuccessHandler;
 import be.kdg.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
- * deze klasse bevat vooral deel 3 van week 6
+ * deze klasse bevat vooral deel 3 van week 5
  */
 
 @Configuration
@@ -32,32 +34,45 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/blog").authenticated()
+                .antMatchers("/", "/blog").authenticated() //welke urls moeten geauthenticeerd worden (voor welke urls moeten men ingelogt zijn)
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll()
+                .loginPage("/login").permitAll()//deze url is toeganklijk voor iedereen & is ook de auto-redirect als men naar vb. "/blog" gaat en niet ingelogt is
+                .successHandler(this.succesHandler()) //zorgt voor een afhandeling van inloggen (in dit geval een redirect naar /blog)
                 .and()
                 .logout()
                 .permitAll();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    /*
+    Deze methode gaar er voorzorgen dat er een LogginSuccessHandler (die de login gaat afhandel) wordt gemaakt
+    we geven de url mee naar waar we de gebruiker willen redirecten als hij inlogt
+     */
+    private AuthenticationSuccessHandler succesHandler() {
+        return new LoginSuccessHandler("/blog");
     }
 
+    //DEEL 3
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //deze config methode wordt gebruikt om eventuele password encoders in te stellen
+        auth.authenticationProvider(this.authProvider());
+    }
+
+    //DEEL 3
     @Bean
     public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(this.userService);
-        authProvider.setPasswordEncoder(passwordEncoder());
+        authProvider.setPasswordEncoder(this.passwordEncoder());
         return authProvider;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
+    //DEEL 3
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
